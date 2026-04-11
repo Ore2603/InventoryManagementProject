0ab75c245337dee71efb6fa5b531898f821f5d08
Ore2603 <69030032+Ore2603@users.noreply.github.com>
2026-04-10 20:23:17 -0400

    Add Menu class with menu display and inventory handling

--- menu.h
+++ menu.h
@@ -1,1 +1,14 @@
+#pragma once
+#include "inventory.h"
 
+class Menu {
+public:
+    void displayMenu();
+
+private:
+    Inventory m_inventory;
+    void handleAdd();
+    void handleRemove();
+    void handleUpdate();
+    void handleSearch();
+};
